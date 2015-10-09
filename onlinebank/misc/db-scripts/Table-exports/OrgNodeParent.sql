d03b62603869748e901a0bc9eb7b34cc50c11d18
kingshukchakraborty <kingshuk.c@tcs.com>
2015-10-09 15:59:33 +0530

    10/09-- Data models extraction from oracle kingshuk-chakraborty

---
+++ onlinebank/misc/db-scripts/Table-exports/OrgNodeParent.sql
@@ -0,0 +1,36 @@
+insert into org_node_parent (PARENT_ORG_NODE_ID, ORG_NODE_ID, CUSTOMER_ID, CREATED_BY, CREATED_DATE_TIME, UPDATED_BY, UPDATED_DATE_TIME, DATA_IMPORT_HISTORY_ID)
+values (981715, 981701, 17013, 228142, to_date('03-04-2015 12:45:11', 'dd-mm-yyyy hh24:mi:ss'), null, to_date('03-04-2015 12:45:11', 'dd-mm-yyyy hh24:mi:ss'), null);
+
+insert into org_node_parent (PARENT_ORG_NODE_ID, ORG_NODE_ID, CUSTOMER_ID, CREATED_BY, CREATED_DATE_TIME, UPDATED_BY, UPDATED_DATE_TIME, DATA_IMPORT_HISTORY_ID)
+values (766873, 766875, 17013, 228142, to_date('31-01-2015 08:25:14', 'dd-mm-yyyy hh24:mi:ss'), null, to_date('31-01-2015 08:25:14', 'dd-mm-yyyy hh24:mi:ss'), null);
+
+insert into org_node_parent (PARENT_ORG_NODE_ID, ORG_NODE_ID, CUSTOMER_ID, CREATED_BY, CREATED_DATE_TIME, UPDATED_BY, UPDATED_DATE_TIME, DATA_IMPORT_HISTORY_ID)
+values (766872, 766873, 17013, 228142, to_date('31-01-2015 07:04:47', 'dd-mm-yyyy hh24:mi:ss'), null, to_date('31-01-2015 07:04:47', 'dd-mm-yyyy hh24:mi:ss'), null);
+
+insert into org_node_parent (PARENT_ORG_NODE_ID, ORG_NODE_ID, CUSTOMER_ID, CREATED_BY, CREATED_DATE_TIME, UPDATED_BY, UPDATED_DATE_TIME, DATA_IMPORT_HISTORY_ID)
+values (2, 766871, 17013, 160040, to_date('31-01-2015 06:54:42', 'dd-mm-yyyy hh24:mi:ss'), null, to_date('31-01-2015 06:54:42', 'dd-mm-yyyy hh24:mi:ss'), null);
+
+insert into org_node_parent (PARENT_ORG_NODE_ID, ORG_NODE_ID, CUSTOMER_ID, CREATED_BY, CREATED_DATE_TIME, UPDATED_BY, UPDATED_DATE_TIME, DATA_IMPORT_HISTORY_ID)
+values (766872, 766874, 17013, 228142, to_date('31-01-2015 07:05:08', 'dd-mm-yyyy hh24:mi:ss'), null, to_date('31-01-2015 07:05:08', 'dd-mm-yyyy hh24:mi:ss'), null);
+
+insert into org_node_parent (PARENT_ORG_NODE_ID, ORG_NODE_ID, CUSTOMER_ID, CREATED_BY, CREATED_DATE_TIME, UPDATED_BY, UPDATED_DATE_TIME, DATA_IMPORT_HISTORY_ID)
+values (766874, 766876, 17013, 228142, to_date('31-01-2015 08:26:07', 'dd-mm-yyyy hh24:mi:ss'), null, to_date('31-01-2015 08:26:07', 'dd-mm-yyyy hh24:mi:ss'), null);
+
+insert into org_node_parent (PARENT_ORG_NODE_ID, ORG_NODE_ID, CUSTOMER_ID, CREATED_BY, CREATED_DATE_TIME, UPDATED_BY, UPDATED_DATE_TIME, DATA_IMPORT_HISTORY_ID)
+values (766871, 766872, 17013, 228142, to_date('31-01-2015 07:03:56', 'dd-mm-yyyy hh24:mi:ss'), null, to_date('31-01-2015 07:03:56', 'dd-mm-yyyy hh24:mi:ss'), null);
+
+insert into org_node_parent (PARENT_ORG_NODE_ID, ORG_NODE_ID, CUSTOMER_ID, CREATED_BY, CREATED_DATE_TIME, UPDATED_BY, UPDATED_DATE_TIME, DATA_IMPORT_HISTORY_ID)
+values (766871, 766907, 17013, 228142, to_date('31-01-2015 07:04:23', 'dd-mm-yyyy hh24:mi:ss'), null, to_date('31-01-2015 07:04:23', 'dd-mm-yyyy hh24:mi:ss'), null);
+
+insert into org_node_parent (PARENT_ORG_NODE_ID, ORG_NODE_ID, CUSTOMER_ID, CREATED_BY, CREATED_DATE_TIME, UPDATED_BY, UPDATED_DATE_TIME, DATA_IMPORT_HISTORY_ID)
+values (766873, 766908, 17013, 228142, to_date('31-01-2015 08:25:43', 'dd-mm-yyyy hh24:mi:ss'), null, to_date('31-01-2015 08:25:43', 'dd-mm-yyyy hh24:mi:ss'), null);
+
+insert into org_node_parent (PARENT_ORG_NODE_ID, ORG_NODE_ID, CUSTOMER_ID, CREATED_BY, CREATED_DATE_TIME, UPDATED_BY, UPDATED_DATE_TIME, DATA_IMPORT_HISTORY_ID)
+values (766874, 766909, 17013, 228142, to_date('31-01-2015 08:26:31', 'dd-mm-yyyy hh24:mi:ss'), null, to_date('31-01-2015 08:26:31', 'dd-mm-yyyy hh24:mi:ss'), null);
+
+insert into org_node_parent (PARENT_ORG_NODE_ID, ORG_NODE_ID, CUSTOMER_ID, CREATED_BY, CREATED_DATE_TIME, UPDATED_BY, UPDATED_DATE_TIME, DATA_IMPORT_HISTORY_ID)
+values (766907, 981715, 17013, 228142, to_date('03-04-2015 12:44:45', 'dd-mm-yyyy hh24:mi:ss'), null, to_date('03-04-2015 12:44:45', 'dd-mm-yyyy hh24:mi:ss'), null);
+
+insert into org_node_parent (PARENT_ORG_NODE_ID, ORG_NODE_ID, CUSTOMER_ID, CREATED_BY, CREATED_DATE_TIME, UPDATED_BY, UPDATED_DATE_TIME, DATA_IMPORT_HISTORY_ID)
+values (766871, 981714, 17013, 228142, to_date('03-04-2015 12:42:11', 'dd-mm-yyyy hh24:mi:ss'), null, to_date('03-04-2015 12:42:11', 'dd-mm-yyyy hh24:mi:ss'), null);
+
---
+++ onlinebank/misc/db-scripts/Table-exports/OrgNodeParent2.sql
@@ -0,0 +1,6 @@
+insert into org_node_parent (PARENT_ORG_NODE_ID, ORG_NODE_ID, CUSTOMER_ID, CREATED_BY, CREATED_DATE_TIME, UPDATED_BY, UPDATED_DATE_TIME, DATA_IMPORT_HISTORY_ID)
+values (1, 2, 1, 1, to_date('08-02-2006', 'dd-mm-yyyy'), null, to_date('08-02-2006', 'dd-mm-yyyy'), null);
+
+insert into org_node_parent (PARENT_ORG_NODE_ID, ORG_NODE_ID, CUSTOMER_ID, CREATED_BY, CREATED_DATE_TIME, UPDATED_BY, UPDATED_DATE_TIME, DATA_IMPORT_HISTORY_ID)
+values (2, 766871, 17013, 160040, to_date('31-01-2015 06:54:42', 'dd-mm-yyyy hh24:mi:ss'), null, to_date('31-01-2015 06:54:42', 'dd-mm-yyyy hh24:mi:ss'), null);
+
