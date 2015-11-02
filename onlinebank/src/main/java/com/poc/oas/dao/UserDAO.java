1587e0d77c559e0a27113a6a2c508ec96b78f2f2
KINGSHUK <ckingshuk5@gmail.com>
2015-11-03 01:33:09 +0530

    11/03 Transaction implementation WIP  ckingshuk5@gmail.com

--- onlinebank/src/main/java/com/poc/oas/dao/UserDAO.java
+++ onlinebank/src/main/java/com/poc/oas/dao/UserDAO.java
@@ -15,6 +15,7 @@ import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.poc.common.CommonConstants;
 import com.poc.oas.domain.UserBean;
